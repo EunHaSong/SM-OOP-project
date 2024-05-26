6e6a2f91c7309c318001809391bc5ecf575df157
lyeonsl <165137719+lyeonsl@users.noreply.github.com>
2024-05-26 10:30:35 +0900

    Add files via upload

---
+++ DB.java
@@ -0,0 +1,20 @@
+import javax.swing.ImageIcon;
+
+class DB {
+    
+    ImageIcon signIcon = new ImageIcon("C:\\yeons\\java\\final\\images\\sign.png");
+    String name = "포36거리 숙대점";
+    String rsInformation = "서울 용산구 청파로47길 77 2층 (02-701-4328)";
+
+    String[] imagePaths = {
+        "C:\\yeons\\java\\final\\images\\menu1.png", "C:\\yeons\\java\\final\\images\\menu2.png", "C:\\yeons\\java\\final\\images\\menu3.png",
+        "C:\\yeons\\java\\final\\images\\menu4.png", "C:\\yeons\\java\\final\\images\\menu5.png", "C:\\yeons\\java\\final\\images\\menu6.png",
+        "C:\\yeons\\java\\final\\images\\menu7.png", "C:\\yeons\\java\\final\\images\\menu8.png", "C:\\yeons\\java\\final\\images\\menu9.png"
+    };
+
+    String [] menudata = {
+        "소고기쌀국수" + " (8000원)", "로스가스 " + " (8000원)", "히레가스 " + " (9000원))",
+        "포돈정식" + " (9500원)", "감자말이 새우롤4ps" + " (5000원)", "칠리소스 닭어깨튀김4ps" + " (5000원)",
+        "새우춘권4ps" + " (5000원)", "소고기쌀국수(어린이)" + " (5000원)", "미니 히레가스(어린이)" + " (5000원)"
+    };
+}
