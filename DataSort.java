a1e046d45766a22ada99320e4f43e893f206cfc9
Mina Choi <165622792+MinaSeoul@users.noreply.github.com>
2024-06-15 14:42:31 +0900

    Update DataSort.java
    폰트 수정

--- DataSort.java
+++ DataSort.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 
+
 public class DataSort extends JFrame {
     Toolkit toolkit = Toolkit.getDefaultToolkit();
     Dimension screenSize = toolkit.getScreenSize();
@@ -14,25 +15,23 @@ public class DataSort extends JFrame {
         setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
 
         JLabel label = new JLabel("정렬");
-        label.setFont(new Font("Helvetica", Font.PLAIN, 20));
+        label.setFont(new Font("NPS font", Font.PLAIN, 20));
         label.setAlignmentX(Component.CENTER_ALIGNMENT);
         label.setBorder(BorderFactory.createEmptyBorder(10, 0, 0, 0)); // 위쪽 여백 추가
 
         add(label);
 
         JButton b1 = new JButton("평점순");
+        b1.setFont(new Font("NPS font", Font.PLAIN, 15));
         b1.setAlignmentX(Component.CENTER_ALIGNMENT);
         add(b1);
 
         JButton b2 = new JButton("리뷰 많은 순");
+        b2.setFont(new Font("NPS font", Font.PLAIN, 15));
         b2.setAlignmentX(Component.CENTER_ALIGNMENT);
         add(b2);
 
         setVisible(true);
-    }
 
-    public static void main(String[] args) {
-        new DataSort();
     }
 }
-
